02216ed8a17448164e46ac789d2963e7c8a3e53e
JaviToro <havitoro95@gmail.com>
2021-03-01 23:28:13 +0100

    CORS updated

--- src/main/java/com/palmatoro/cmmimplant/controller/UserController.java
+++ src/main/java/com/palmatoro/cmmimplant/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 @RequestMapping(path = "/user")
 public class UserController {
     @Autowired
