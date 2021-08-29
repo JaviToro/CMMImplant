8c7a28d9790408840f812a44c0b5eecc6a0b48b1
PabloP <pablopr.95@hotmail.com>
2021-08-29 20:40:16 +0200

    ValueController

--- src/main/java/com/palmatoro/cmmimplant/controller/ValueController.java
+++ src/main/java/com/palmatoro/cmmimplant/controller/ValueController.java
@@ -3,6 +3,7 @@ package com.palmatoro.cmmimplant.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.palmatoro.cmmimplant.domain.Metric;
 import com.palmatoro.cmmimplant.domain.Value;
 import com.palmatoro.cmmimplant.exception.ResourceNotFoundException;
 import com.palmatoro.cmmimplant.service.UserService;
@@ -19,7 +20,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -40,8 +40,8 @@ public class ValueController {
 
     @GetMapping(path = "/list")
     @Secured({"ROLE_USER", "ROLE_PM", "ROLE_ADMIN"})
-    @RequestMapping(value = {"/all", "/all/error/{code}"}, method = RequestMethod.GET)
-    public String list(Model model, @PathVariable(value = "code", required = false) Integer errorCode) {
+    @RequestMapping(value = {"/metric/{id}", "/metric{id}/error/{code}"}, method = RequestMethod.GET)
+    public String list(Model model,@PathVariable(value = "id", required = true) Integer metricId, @PathVariable(value = "code", required = false) Integer errorCode) {
 
         if (errorCode != null){
             model.addAttribute("error", "Se ha producido un error");
@@ -58,7 +58,8 @@ public class ValueController {
         if(isAdmin==true){
             results = (List<Value>) valueService.getAllValues();
         }else{
-            results = userService.getUserByUsername(authentication.getName()).getProject().getMetrics().get
+            Metric m = userService.getUserByUsername(authentication.getName()).getProject().getMetrics().get(metricId);
+            results = m.getValues();
         }
 
         model.addAttribute("results", results);
@@ -73,22 +74,24 @@ public class ValueController {
         return valueService.getValueById(id);
     }
 
-    @RequestMapping(value = {"/add", "/add/{id}"}, method = RequestMethod.GET)    
+    @RequestMapping(value = {"/add/{metricId}", "/add/{id}"}, method = RequestMethod.GET)    
     @Secured({"ROLE_USER", "ROLE_PM", "ROLE_ADMIN"})
-    public String addNew(Model model, @PathVariable(value = "id", required = false) Integer id) {
+    public String addNew(Model model, @PathVariable(value = "metricId", required = false) Integer metricId, @PathVariable(value = "id", required = false) Integer id) {
 
         if (id != null) {
             model.addAttribute("result", valueService.getValueById(id));
         } else {
             model.addAttribute("result", new Value());
+            model.addAttribute("metricId", metricId);
         }
 
         return "value/add";
     }
 
-    @PostMapping("/add")
+    @RequestMapping(value = {"/add/{metricId}", "/add/{id}"}, method = RequestMethod.POST)  
     @Secured({"ROLE_USER", "ROLE_PM", "ROLE_ADMIN"})
-    public String addNew(@ModelAttribute("projectForm") Value result, BindingResult bindingResult) {
+    public String addNew(@ModelAttribute("projectForm") Value result, BindingResult bindingResult,
+    @PathVariable(value = "metricId", required = false) Integer metricId, @PathVariable(value = "id", required = false) Integer id) {
         valueValidator.validate(result, bindingResult);
 
         if (bindingResult.hasErrors()) {
@@ -98,7 +101,7 @@ public class ValueController {
         if (result.getId() != null) {
             valueService.editValue(result.getId(), result.getIdentifier(), result.getMoment(), result.getAmount());
         } else {
-            valueService.addNewValue(result);
+            valueService.addNewValue(result, metricId);
         }
 
         return "redirect:/value/list";
