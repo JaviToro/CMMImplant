16cb3c699ce88168c312c5d9df9339bf4c1da7d0
PabloP <pablopr.95@hotmail.com>
2021-08-29 20:01:04 +0200

    Metric Controller

---
+++ src/main/java/com/palmatoro/cmmimplant/validator/MetricValidator.java
@@ -0,0 +1,36 @@
+package com.palmatoro.cmmimplant.validator;
+
+import com.palmatoro.cmmimplant.domain.Metric;
+import com.palmatoro.cmmimplant.domain.User;
+import com.palmatoro.cmmimplant.service.MetricService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class MetricValidator implements Validator {
+
+    @Autowired
+    private MetricService metricService;
+
+    @Override
+    public boolean supports(Class<?> aclass) {
+        return User.class.equals(aclass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Metric result = (Metric) o;
+
+        for(Metric m: metricService.getAllMetrics()){
+            if(m.getIdentifier().equals(result.getIdentifier()) && result.getId()!=null){
+                errors.rejectValue("identifier", "DuplicatedIdentifier");
+            }
+            if(m.getName().equals(result.getName()) && result.getId()!=null){
+                errors.rejectValue("name", "DuplicatedName");
+            }
+        }
+    }
+}
