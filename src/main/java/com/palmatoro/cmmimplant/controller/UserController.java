c5e5297a7c25d13f2fa0325d19bd0f8d2fb39d9a
PabloP <pablopr.95@hotmail.com>
2021-08-31 02:22:12 +0200

    Unused imports deleted

--- src/main/java/com/palmatoro/cmmimplant/controller/UserController.java
+++ src/main/java/com/palmatoro/cmmimplant/controller/UserController.java
@@ -18,7 +18,6 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
